dd34bde6b3a15bea77530b3bbb092265d8db482a
adrian <jeisondevs@gmail.com>
2025-10-17 22:46:21 -0400

    quite el archibo de firebase

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.use((req, res, next) => {
-    //res.json({ message: 'Postback recibido!!'});
-console.log(req.method);
-    next();
-});
 
 
 app.get('/', (req, res) => {
