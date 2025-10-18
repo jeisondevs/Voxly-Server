e4600429351fdb6cd500f58f1a8aebb0be2234ed
adrian <jeisondevs@gmail.com>
2025-10-17 21:56:33 -0400

    Add Ruta no encontrada y carpeta Middlewares

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Bienvenido al servidor de postback!!');
+app.use((req, res, next) => {
+    //res.json({ message: 'Postback recibido!!'});
+console.log(req.method);
+    next();
 });
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Bienvenido al servidor de Postback'});
+});
+
+import notFound from "./src/middlewares/not-found.js";
+app.use(notFound);
 
 app.listen(PORT, () => console.log('http://localhost:+{PORT}'));
